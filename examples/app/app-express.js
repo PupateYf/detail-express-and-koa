0daffc3282329d4d65e08c331d68b045e5328368
PupateYf <ice-4.ever@163.com>
2017-12-19 23:42:04 +0800

    feat: add error handle

--- examples/app/app-express.js
+++ examples/app/app-express.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res, next) => {
+app.use((req, res, next) => {
     res.send('Hey guys -express')
 })
 
-//托管静态资源
+//处理错误
+app.use((err,req,res,next) => {
+    // handle error here
+})
 
 
 var server = app.listen(3333)
--- examples/app/app-koa.js
+++ examples/app/app-koa.js
@@ -5,7 +5,9 @@ app.use(async ctx => {
   ctx.body = 'Hey guys -koa'
 })
 
-//托管静态资源
-
+//处理错误
+app.on('error', (ctx, next) => {
+  //handle error here
+})
 
 app.listen(3333)
